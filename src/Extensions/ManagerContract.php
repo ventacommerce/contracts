4e1690ba1510006772fcf485613fa8b226ea9ce9
Konstantins Zukovs <konstantins@majaslapa.lv>
2016-05-05 17:09:15 +0300

    Updates for extensions manager contracts

--- src/Extensions/ManagerContract.php
+++ src/Extensions/ManagerContract.php
@@ -16,34 +16,12 @@ interface ManagerContract
      * @param int $priority
      */
     public function addExtensionProvider(ProviderContract $provider, int $priority = null);
-
-    /**
-     * Add new data loading method for extension loader to support
-     *
-     * @param  MethodContract $method
-     */
-    public function addMethod(MethodContract $method);
-
-    /**
-     * Defines, if method exists
-     *
-     * @param  string $name
-     * @return bool
-     */
-    public function hasMethod(string $name): bool;
-
-    /**
-     * Alias for hasMethod()
-     *
-     * @param  string $name
-     * @return bool
-     */
-    public function supports(string $name): bool;
-
+    
     /**
      * Calls method on extension provider
      *
-     * @param string $name
+     * @param  string $name
+     * @param  array ...$arguments
      */
-    public function call(string $name);
+    public function call(string $name, ...$arguments);
 }
